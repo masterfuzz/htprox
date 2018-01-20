96ee5823f7b21450cd4d4c295dd135ba8adac28a
Rob Martens <master.fuzz@gmail.com>
2018-01-20 00:28:01 -0500

    gateway http register proto

--- htp_gateway/htp_gateway.go
+++ htp_gateway/htp_gateway.go
@@ -1,22 +1,38 @@
 package htp_gateway
 
 import (
-    "net"
+    "net/http"
+    "log"
+    "fmt"
 )
 
 type Gateway struct {
     listenAddr string
-    listenConn net.Conn
     // endpoints
 }
 
 func NewGateway(listen string) Gateway {
     c := Gateway{
-        listenAddr: listen,
-        listenConn: nil}
+        listenAddr: listen}
     return c
 }
 
-func (c Gateway) Run() {
+func (g Gateway) Run() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+    })
+    http.HandleFunc("/register", g.httpRegister)
+
+    log.Printf("Listening on %v", g.listenAddr)
+    log.Fatal(http.ListenAndServe(g.listenAddr, nil))
 }
 
+func (g *Gateway) httpRegister(w http.ResponseWriter, r *http.Request) {
+    log.Printf("%v: %v", r.Method, r.URL.Path)
+    name := r.FormValue("name")
+
+    log.Printf("Register: %v", name)
+}
+
+func (g *Gateway) Register(endpoint string) {
+}
